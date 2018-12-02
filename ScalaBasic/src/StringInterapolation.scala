70bcab5f40fb161360080bcb159cc6944dcae113
sathishrpt24 <sathishrpt24@gmail.com>
2018-12-02 06:52:10 +0530

    First Scala Programm

---
+++ ScalaBasic/src/StringInterapolation.scala
@@ -0,0 +1,22 @@
+
+
+object StringInterapolation {
+  
+  def main(args:Array[String]){
+    
+    val a =10;
+    
+    var b = 10;
+    
+    val d = "Sathish Ponnusamy";
+    
+    println(d);
+    
+    println("The value of a is - " + a);
+    
+    println("The value of b is +:"+b);
+    
+    println("The value of d is"+d);
+      
+  }
+}
