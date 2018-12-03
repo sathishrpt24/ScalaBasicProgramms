2ecaf80cad3bf35fb748fa3641d06f941afe0a1d
sathishrpt24 <sathishrpt24@gmail.com>
2018-12-03 06:11:52 +0530

    - ForLoop
    - For with Collections
    - For Loop with Filters

--- ScalaBasic/src/ForLoop.scala
+++ ScalaBasic/src/ForLoop.scala
@@ -12,6 +12,8 @@ object ForLoop {
 
     }
     //untill method
+    
+    //ForLoop with ranges
     for (x <- 1 until 10) {
 
       println("X value using until method - " + x)
@@ -27,6 +29,31 @@ object ForLoop {
       println("X value using nested forloop - " + x)
 
     }
+
+    // For loop with if condition
+    
+    println("*********************")
+    
+    //For Loop with filters
+    
+    for (x <- 1 to 10 if (x != 0); if (x == 5)) {
+
+      println("The value of x is:" + x);
+    }
+
+    // List for loop in Scala
+
+    var tempList = List(1, 2, 34, 4, 5, 6, 7, 89)
+
+    // Forloop with collections
+    for (x <- tempList if (x != 0); if (x <= 5)) {
+      println("The value of a from list is-" + x)
+    }
+
+    // ForLoop with yield method
+   var newList =  for{x <- tempList if (x != 0); if (x <= 5)} yield x
+   
+   println("The value of new list is:- "+ newList)
   }
 
 }
